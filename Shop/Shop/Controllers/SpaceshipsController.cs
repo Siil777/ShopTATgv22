9358839365d95fef5e63d2741f1b8dd31b2ca6ef
Siil777 <pavel.ivanov.thk@gmail.com>
2023-09-05 14:47:13 +0300

    Init
    initial information

---
+++ Shop/Shop/Controllers/SpaceshipsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Shop.Controllers
+{
+    public class SpaceshipsController : Controller
+    {
+
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
