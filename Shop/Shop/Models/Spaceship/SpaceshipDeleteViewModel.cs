863d97c0ca530d7a0a7bfcf57d589e34354a3c99
Siil777 <pavel.ivanov.thk@gmail.com>
2023-09-18 19:15:15 +0300

    SpaceShip 2.2
    class to delete with data types of information we will call by deleting some data.

---
+++ Shop/Shop/Models/Spaceship/SpaceshipDeleteViewModel.cs
@@ -0,0 +1,26 @@
+﻿namespace Shop.Models.Spaceship
+{
+    public class SpaceshipDeleteViewModel
+    {
+        public Guid? Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+
+        public int Passangers { get; set; }
+
+        public int EnginPower { get; set; }
+
+        public int Crew { get; set; }
+
+        public string Company { get; set; }
+
+        public int CargoWeight { get; set; }
+
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime Modifieted { get; set; }
+    }
+}
