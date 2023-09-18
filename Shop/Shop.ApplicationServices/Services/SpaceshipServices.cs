f6066041aa37d196ed90b7284d92ebe2b6916492
Siil777 <pavel.ivanov.thk@gmail.com>
2023-09-18 08:18:04 +0300

    SpaceShip1
    this code defines a service class for creating spaceship objects.

---
+++ Shop/Shop.ApplicationServices/Services/SpaceshipServices.cs
@@ -0,0 +1,44 @@
+﻿using Shop.Core.Domain;
+using Shop.Core.Dto;
+using Shop.Core.ServiceInterface;
+using Shop.Data;
+
+namespace Shop.ApplicationServices.Services
+{
+    public class SpaceshipServices : ISpaceshipServices
+    {
+
+        private readonly ShopContext _context;
+
+        public SpaceshipServices 
+            (
+                ShopContext context
+            )
+        {
+            _context=context;
+        }
+
+        public async Task<Spaceship>  Create(SpaceshipDto dto)
+        {
+            Spaceship spaceship= new Spaceship();   
+
+            spaceship.Id= Guid.NewGuid();
+            spaceship.Name = dto.Name;
+            spaceship.Type = dto.Type;
+            spaceship.Passangers = dto.Passangers;
+            spaceship.EnginPower = dto.EnginPower;
+            spaceship.Crew= dto.Crew; 
+            spaceship.Company= dto.Company;
+            spaceship.CargoWeight= dto.CargoWeight; 
+            spaceship.CreatedAt= DateTime.Now; 
+            spaceship.Modifieted= DateTime.Now;
+
+            await _context.Spaceships.AddAsync( spaceship );
+            await _context.SaveChangesAsync();
+
+
+
+            return spaceship;
+        }
+    }
+}
