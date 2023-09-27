83a07dbe7b5b9531b74e7da64cce2b80ec7e6ea0
Siil777 <pavel.ivanov.thk@gmail.com>
2023-09-27 16:52:23 +0300

    SpaceShip 3.1
    1. this class helps encapsulate spaceship-related data and images, making it easier to pass this information between different layers or components of an application, such as between a controller and a service.

--- Shop/Shop.Core/Dto/SpaceshipDto.cs
+++ Shop/Shop.Core/Dto/SpaceshipDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -29,6 +30,12 @@ namespace Shop.Core.Dto
         public DateTime Modifieted { get; set; }
 
         //only in database
+          //works with img 
+        public List<IFormFile> Files { get; set; }
+
+        public IEnumerable<FileToApiDto> Image { get; set; }
+
+        = new List<FileToApiDto>();
 
     }
 }
