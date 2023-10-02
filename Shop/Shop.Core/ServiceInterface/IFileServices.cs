ee592efec3cf7f40f9dc22f27614f5d14bc2fcf8
Siil777 <pavel.ivanov.thk@gmail.com>
2023-10-02 21:11:37 +0300

    SpaceShip 3.6 FileServices
    1. store method appears  for removing multiple images asynchronously.  Task<List<FileToApi>> RemoveImagesFromApi(FileToApiDto[] dtos);
    
    2.Second task designed for removing a single image

--- Shop/Shop.Core/ServiceInterface/IFileServices.cs
+++ Shop/Shop.Core/ServiceInterface/IFileServices.cs
@@ -11,6 +11,10 @@ namespace Shop.Core.ServiceInterface
     public interface IFileServices
     {
         void FilesToApi(SpaceshipDto dto, Spaceship spaceship);
+                          //array a few file at the same time
+        Task<List<FileToApi>> RemoveImagesFromApi(FileToApiDto[] dtos);
+
+        Task<FileToApi> RemoveImageFromApi(FileToApiDto dto);
 
         //void FilesToApi(RealEstateDto dto, RealEstate realestate);
 
