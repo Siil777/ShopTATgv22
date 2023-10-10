fc40f3747698319af8243a4e0ccb17989bdc1e22
Siil777 <pavel.ivanov.thk@gmail.com>
2023-10-10 20:29:28 +0300

    IKinderGartnerServices 4.1
    1.to managing kindergarten data within an application.
    2.perform CRUD (Create, Read, Update, Delete) operations on kindergarten entities.

---
+++ Shop/Shop.Core/ServiceInterface/IKindergartenServices.cs
@@ -0,0 +1,21 @@
+﻿using Shop.Core.Domain;
+using Shop.Core.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Core.ServiceInterface
+{
+    public interface IKindergartenServices
+    {
+        Task<KinderGarten> Create(KinderGartenDto dto);
+
+        Task<KinderGarten> GetAsync(Guid id);
+
+        Task<KinderGarten> Update(KinderGartenDto dto);
+
+        Task<KinderGarten> Delete(Guid id);
+    }
+}
