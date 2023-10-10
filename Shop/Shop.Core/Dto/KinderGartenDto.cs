f421accef6b59661620e026f2ff7e2ab8e168ae1
Siil777 <pavel.ivanov.thk@gmail.com>
2023-10-10 20:27:04 +0300

    KinderGartenDto 4.0
    1.DTO  data when passing it between different layers of an application or between the client and server.

---
+++ Shop/Shop.Core/Dto/KinderGartenDto.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Core.Dto
+{
+    public class KinderGartenDto
+    {
+        public Guid? Id { get; set; }
+        public string GroupName { get; set; }
+
+        public int ChildrenCount { get; set; }
+
+        public string KindergartenName { get; set; }
+
+        public string Teacher { get; set; }
+
+        public List<IFormFile> Files { get; set; }
+
+
+
+        //database
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
