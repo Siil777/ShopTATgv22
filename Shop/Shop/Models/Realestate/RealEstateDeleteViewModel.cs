fbf67507942fc17b90e333855572bd9e0cbe3fcd
Siil777 <pavel.ivanov.thk@gmail.com>
2023-10-19 11:07:15 +0300

    RealEstateDeleteViewModel.cs 5.9
    1.imageToDatabaseViewModels list provides a structured way to manage and display images associated with a real estate .

--- Shop/Shop/Models/Realestate/RealEstateDeleteViewModel.cs
+++ Shop/Shop/Models/Realestate/RealEstateDeleteViewModel.cs
@@ -14,5 +14,7 @@
 
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
+
+        public List<ImageToDatabaseViewModel> imageToDatabaseViewModels { get; set; } = new List<ImageToDatabaseViewModel>();   
     }
 }
