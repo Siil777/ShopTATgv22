776a5b46927a4af34e253b517297fc60956c3433
Siil777 <pavel.ivanov.thk@gmail.com>
2023-10-19 11:00:03 +0300

    Shop.SpaceshipTest 5.6
    1. Testing spaceship-related methods behave as expected in different scenarios.
    
    2.(ShouldNot_GetByIdSpaceship_WhenReturnNotEqual), we need to complete the test by defining specific assertions that check the behavior of the GetById method with valid and invalid GUIDs. This might involve checking if the method returns a spaceship with the expected properties or if it returns null or throws an exception when given an invalid GUID.

---
+++ Shop/Shop.SpaceshipTest/SpaceShipTest.cs
@@ -0,0 +1,54 @@
+﻿using Shop.Core.Dto;
+using Shop.Core.ServiceInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.SpaceshipTest
+{
+    public class SpaceShipTest : TestBase
+    {
+        [Fact]
+        public async Task ShouldNot_AddEmptySpaceship_WhenReturnresult()
+        {
+
+
+            SpaceshipDto dto= new SpaceshipDto(); 
+
+            dto.Name = "Name";
+            dto.Type = "Type";
+            dto.Passangers = 123;
+            dto.EnginPower = 123;
+            dto.Crew = 123;
+            dto.Company = "asd";
+            dto.CargoWeight = 123;
+
+            dto.CreatedAt= DateTime.Now;
+            dto.Modifieted=DateTime.Now;
+
+            var result = await Svc<ISpaceshipServices>().Create(dto);
+
+            Assert.NotNull(result);
+
+        }
+
+        [Fact] 
+        //chack a path for elements
+        public async Task ShouldNot_GetByIdSpaceship_WhenReturnNotEqual()
+
+        {
+            Guid guid = Guid.Parse("67457d6e-854d-4112-b467-776ef280574c");
+            //kuidas teha automaatselt teeb guidi ???
+            Guid wrongGuid =Guid.Parse(Guid.NewGuid().ToString());
+
+            //Guid Wronggugid = Guid.NewGuid();
+            //Random random = new Random();
+            //int i = random.Next();
+        }
+
+
+
+    }
+}
