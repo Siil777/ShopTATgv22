e3ff8a6e6770f855a089706254c9b33b1cfc6c97
Siil777 <pavel.ivanov.thk@gmail.com>
2023-10-30 18:08:48 +0200

    KinderGarden MockEnvironment 6.4
    IHostEnvironment interface represents the hosting environment for the application. It provides information about the environment in which the application is running, such as the environment name, application name, content root path, and file providers.

---
+++ Shop/Shop.KindergartenTest/Mock/MockHostEnvironment.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.KindergartenTest.Mock
+{
+    public class MockHostEnvironment : IHostEnvironment
+    {
+        string IHostEnvironment.EnvironmentName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        string IHostEnvironment.ApplicationName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        string IHostEnvironment.ContentRootPath { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        IFileProvider IHostEnvironment.ContentRootFileProvider { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
