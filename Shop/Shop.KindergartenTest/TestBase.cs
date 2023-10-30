be2bbc8eac2ecea00ca3ba16d1a40e637ceea49d
Siil777 <pavel.ivanov.thk@gmail.com>
2023-10-30 18:06:19 +0200

    KinderGarten TestBase  6.3
    1. ServiceProvider instance of IServiceProvider. provide access to various services and dependencies within application.
    
    2.constructor of the TestBase class sets up the service collection (ServiceCollection) and builds the service provider.
    
    3. Dispose - clean up unmanaged resources.
    
    4. Svc<T> Method: This generic method is used to obtain a service of type T from the service provider
    
    5. SetupServices Method: This method is responsible for configuring the services in the service collection
    
    6. RegisterMacros Method: This method scans for types that implement the IMacros interface within the same assembly
    
    Testbase - set up a testing environment for unit test.

---
+++ Shop/Shop.KindergartenTest/TestBase.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Shop.ApplicationServices.Services;
+using Shop.Core.ServiceInterface;
+using Shop.Data;
+using Shop.KindergartenTest.Macros;
+using Shop.KindergartenTest.Mock;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.KindergartenTest
+{
+    public class TestBase
+    {
+        protected IServiceProvider ServiceProvider { get; }
+
+        protected TestBase() 
+        {
+            var services=new ServiceCollection();
+            SetupServices(services);
+            ServiceProvider = services.BuildServiceProvider();
+        }
+        public void Dispose() 
+        { 
+
+        }
+        protected T Svc<T> ()
+        {
+            return ServiceProvider.GetService<T>();
+        }
+
+        protected T Macro<T>() where T: IMacros
+        {
+            return ServiceProvider.GetService<T>();
+        }
+
+        public virtual void SetupServices(IServiceCollection services)
+        {
+            services.AddScoped<IKindergartenServices, KinderGartenServices>();
+            services.AddScoped<IFileServices, FileServices>();
+            services.AddScoped<IHostEnvironment, MockHostEnvironment>();
+
+
+
+            services.AddDbContext<ShopContext>(x =>
+            {
+                x.UseInMemoryDatabase("TEST");
+                x.ConfigureWarnings(e=>e.Ignore(InMemoryEventId.TransactionIgnoredWarning));
+
+            });
+            RegisterMacros(services);
+
+        }
+        private void RegisterMacros(IServiceCollection services)
+        {
+            var macrosBaseType = typeof(IMacros);
+
+            var macros = macrosBaseType.Assembly.GetTypes()
+                .Where(x => macrosBaseType.IsAssignableFrom(x) && !x.IsInterface && !x.IsAbstract);
+
+            foreach (var macro in macros)
+            {
+                services.AddSingleton(macro);
+
+            }
+        }
+    }
+}
