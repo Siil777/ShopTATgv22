cf982672cb11416209f445e4a6eb710e6eb41235
Siil777 <pavel.ivanov.thk@gmail.com>
2023-11-19 13:44:31 +0200

    OpenWeatherRespondsRootDto 6.6
    1. get JSON responses from the OpenWeather API

---
+++ Shop/Shop.Core/Dto/OpenWeastherDtos/OpenWeatherRespondsRootDto.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Query.Internal;
+using Newtonsoft.Json;
+using Shop.Core.Dto.OpenWeastherDtos;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using static Shop.Core.Dto.OpenWeastherDtos.WeatherClasses;
+
+namespace Shop.Core.Dto
+{
+    public class OpenWeatherRespondsRootDto
+    {
+        // Root myDeserializedClass = JsonSerializer.Deserialize<Root>(myJsonResponse);
+
+
+        [JsonPropertyName("coord")]
+        public Coord Coord { get; set; }
+
+        [JsonPropertyName("weather")]
+        public List<Weather> Weather { get; set; }
+
+        [JsonPropertyName("base")]
+        public string @base { get; set; }
+
+        [JsonPropertyName("main")]
+        public Main Main { get; set; }
+
+        [JsonPropertyName("visibility")]
+        public int Visibility { get; set; }
+
+        [JsonPropertyName("wind")]
+        public Wind Wind { get; set; }
+
+        [JsonPropertyName("rain")]
+        public Rain Rain { get; set; }
+
+        [JsonPropertyName("snow")]
+        public Snow Snow { get; set; }
+
+        [JsonPropertyName("clouds")]
+        public Clouds Clouds { get; set; }
+
+        [JsonPropertyName("dt")]
+        public int Dt { get; set; }
+
+        [JsonPropertyName("sys")]
+        public Sys Sys { get; set; }
+
+        [JsonPropertyName("timezone")]
+        public int TimeZone { get; set; }
+
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("cod")]
+        public int Cod { get; set; }
+    }
+}
+
