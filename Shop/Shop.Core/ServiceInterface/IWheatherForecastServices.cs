57c77318e34800a0daa8a290780ef458afc00ef0
Siil777 <pavel.ivanov.thk@gmail.com>
2023-11-19 14:24:50 +0200

    IWeatherForecastServices 7.3
    1. an abstraction for the weather forecast service

---
+++ Shop/Shop.Core/ServiceInterface/IWheatherForecastServices.cs
@@ -0,0 +1,15 @@
+﻿using Shop.Core.Dto;
+using Shop.Core.Dto.OpenWeastherDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Core.ServiceInterface
+{
+    public interface IWheatherForecastServices
+    {
+        Task<OpenWeatherResultDto> OpenWeatherResult(OpenWeatherResultDto dto);
+    }
+}
